82195b5f75875e5d1e58141b9963b92f3eb9ff09
Krzysztof Tyszecki <krzysztof@tyszecki.org>
2011-05-21 12:58:19 +0200

    Automatyczne raportowanie błędów FC.

---
+++ src/org/tyszecki/rozkladpkp/RozkladPKPApplication.java
@@ -0,0 +1,16 @@
+package org.tyszecki.rozkladpkp;
+
+import android.app.Application;
+import org.acra.*;
+import org.acra.annotation.*;
+
+@ReportsCrashes(formKey = "dFlJOVYyS1hYbENUWEVmQnE5azlKNFE6MQ")
+
+public class RozkladPKPApplication extends Application {
+	@Override
+    public void onCreate() {
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+        super.onCreate();
+    }
+}
